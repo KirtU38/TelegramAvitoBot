7eaaeca90ffb2ee0a16f855fbf53c14e190914c7
KirtU38 <Supermanegor1994>
2021-03-06 22:59:37 +0700

    AvitoBot with proxy + userAgent 1

--- src/main/java/ru/beloshitsky/telegrambot/parsers/AvitoHTMLParser.java
+++ src/main/java/ru/beloshitsky/telegrambot/parsers/AvitoHTMLParser.java
@@ -46,7 +46,7 @@ public class AvitoHTMLParser {
 
     try {
       long start = System.currentTimeMillis();
-      htmlDoc = Jsoup.connect(URL).userAgent("Mozilla").get();
+      htmlDoc = Jsoup.connect(URL).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36").get();
       long wastedTime = System.currentTimeMillis() - start;
       long sleepTime = botConfig.getDelayBetweenConnections() - wastedTime;
       Thread.sleep(sleepTime < 0 ? 0 : sleepTime);
